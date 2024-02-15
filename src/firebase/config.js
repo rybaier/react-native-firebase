892269d65714bfef7b0173166fc93d65e97ffeb4
rybaier <rybaier@gmail.com>
2024-02-14 22:32:34 -0800

    Add User Persistence
    - Add AsyncStorage from react native async storage package to firebase
    config
    - Persist firebase auth with AsyncStorage and initialize firebase Auth

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { getAuth, getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -35,8 +36,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app)
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+})
 const db = getFirestore(app);
 
 export { app , auth, db }
